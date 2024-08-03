b00efc3070c4b22ebc0ac28d3b171e0a8366eea5
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-03 12:42:40 +0300

    Third homework done ✓

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-class Buyer:
-    """
-    This class represents a buyer
-    """
-
-    def __init__(self, name, surname, address, phone, email):
-        self.name = name
-        self.surname = surname
-        self.address = address
-        self.phone = phone
-        self.email = email
+class Order:
+    def __init__(self, order_no, buyer_no):
+        self.order_no = order_no
+        self.buyer_no = buyer_no
+        self.goods_list = []
 
     def __str__(self):
-        return f'{self.name} {self.surname} \n{self.address} \n{self.phone} \n{self.email}'
+        res = f'order No: {self.order_no}'
+        res += f'\nBuyer No: {self.buyer_no}\n'
+        res += '\n'.join(map(str, self.goods_list))
+        return res
 
 
-buyer1 = Buyer("Oleh", "Radchenko", "My address", "0998877665", "test@test.test")
-print(buyer1)
+order1 = Order(order_no=534872346, buyer_no=6236)
+order1.goods_list = [[1, "Keyboard"], [2, "Mouse"], [3, "Microphone"]]
 
+print(order1)
