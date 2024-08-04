657a9de7edf84f11c1ffbce300df1228a23c4fd3
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-04 12:45:55 +0300

    Lesson 2. Homework task 2 and 3 - Done

--- lesson2.py
+++ lesson2.py
@@ -50,12 +50,81 @@ class D(A, B):
 # HOMEWORK #############################################################################################################
 # task 1
 class Human:
-    def __init__(self, firstname, second_name, surname):
-        self.firstname = firstname
+    def __init__(self, first_name, surname, second_name):
+        self.first_name = first_name
         self.second_name = second_name
         self.surname = surname
 
     def __str__(self):
-        return f'{self.surname} {self.firstname} {self.second_name}'
+        return f'{self.surname} {self.first_name} {self.second_name}\n'
 
+# task 2
+class Student(Human):
+    def __init__(self, first_name, surname, second_name, receipt_date):
+        super().__init__(first_name, surname, second_name)
+        self.receipt_date = receipt_date
 
+    def __str__(self):
+        return f'{self.surname} {self.first_name[0]}. {self.second_name[0]}. \t{self.receipt_date}\n'
+
+# task 3
+class Group():
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) == 10:
+            print(f'To much students in this group. Create the new one.')
+            return False
+        if student not in self.students:
+            self.students.append(student)
+        else:
+            print(f'Student {student} already in this group.')
+
+    def remove_student(self, student):
+        if student in self.students:
+            self.students.remove(student)
+        else:
+            print(f'Student {student} not in this group.')
+
+    def search_student(self, surname):
+        res = []
+        for student in self.students:
+            if student.surname.find(surname) > -1:
+                res.append(student)
+
+        return res
+
+    def __str__(self):
+        res = f'Group: {self.name}\n'
+        res += f'Students list:\n'
+        res += ''.join(map(str, self.students))
+        return res
+
+if __name__ == '__main__':
+    group = Group('410-i')
+
+    students = []
+    students.append(Student("Антків", "Твердогоста" ,"Устимівна", ''))
+    students.append(Student("Трощинська", "Чеслава" ,"Костянтинівна", ''))
+    students.append(Student("Улицька", "Купава" ,"Левівна", ''))
+    students.append(Student("Ерстенюк", "Евеліна" ,"Борисівна", ''))
+    students.append(Student("Забіла", "Радченко" ,"Федорівна", ''))
+    students.append(Student("Марковичі", "Радченко" ,"Соломонівна", ''))
+    students.append(Student("Їжакевич", "Далемир" ,"Тарасович", ''))
+    students.append(Student("Лугова", "Єкатерина" ,"Добромирівна", ''))
+    students.append(Student("Харченко", "Братко" ,"Іванович", ''))
+    students.append(Student("Якобовський", "Ус" ,"Світанович", ''))
+    students.append(Student("Єрченко", "Златодан" ,"Світанович", ''))
+
+    for student in students:
+        group.add_student(student)
+
+    print(group)
+
+    group.remove_student(students[-2])
+
+    print(group)
+
+    print("".join(map(str,group.search_student('Радченко'))))
