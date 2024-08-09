0b1f5ecd41798e4d82d7f3ed56a3d0eaacf0b858
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-09 08:00:23 +0300

    Lesson 7 - Generators. Done

--- lesson7/lesson7.py
+++ lesson7/lesson7.py
@@ -11,11 +11,11 @@ def gen_funk(start, stop):
         index += 1
 
 
-x = func(10, 20)
+# x = func(10, 20)
 # print(x)
 
 
-y = gen_funk(10, 20)
+# y = gen_funk(10, 20)
 # print(y)
 
 # for item in y:
@@ -24,9 +24,26 @@ y = gen_funk(10, 20)
 #####################################
 
 # generator OBJECT >
-z = (i * i for i in range(10))
-print(z)  # generator OBJECT >
-print([i for i in z])
+# z = (i * i for i in range(10))
+
+
+# print(z)  # generator OBJECT >
+# print([i for i in z])
 
 #####################################
 
+def square_gen(n):
+    i = 0
+    exponent = 2
+    while i < n:
+        temp_exponent = (yield i ** exponent)
+        if temp_exponent:
+            exponent = temp_exponent
+        i += 1
+
+
+l = square_gen(20)
+for i in l:
+    print(i)
+    if i > 100:
+        l.send(3)
