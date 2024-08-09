d37d646517ad29c65a1e7d9f33705470e17708aa
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-09 09:26:53 +0300

    Lesson 7 - Generators. Homework task 3 - done

---
+++ lesson7/Hometasks/Hometask_3.py
@@ -0,0 +1,26 @@
+def prime_gen(my_num):
+    for i in range(1, my_num + 1):
+        if i == 1:
+            yield i
+        elif i == 2:
+            yield i
+        elif i % 2 == 0 and i != 2:
+            continue
+        else:
+            counter = 1
+            for n in range(i, 2, -1):
+                if i % n == 0:
+                    counter += 1
+                if counter > 2:
+                    break
+            if counter == 2:
+                yield i
+    return
+
+
+a = prime_gen(100)
+# for i in a:
+#     print(i)
+
+x = list(a)
+print(x)
