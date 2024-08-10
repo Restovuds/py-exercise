c725eb93bb86ef641548479b8f12cad8c2c078a7
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-10 14:39:27 +0300

    Lesson 8 - Homework 2 - Done

---
+++ lesson8/hometasks/hometask_2.py
@@ -0,0 +1,21 @@
+def fibonacci():
+    results = [0, 1,]
+
+    def get_n(n):
+        if n <= len(results):
+            return results[n-1]
+
+        for i in range(len(results), n):
+            next_number = results[i - 1] + results[i - 2]
+            results.append(next_number)
+
+        return results[n-1]
+    return get_n
+
+
+fibo = fibonacci()
+for i in range(1, 100):
+    print(fibo(i))
+
+
+
