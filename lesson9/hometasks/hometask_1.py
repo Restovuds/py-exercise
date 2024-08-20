f4b50ab3bece6c2cc04feeed2638294f5c4266e4
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-20 22:52:14 +0300

    Lesson 9 - Decorators homework check

--- lesson9/hometasks/hometask_1.py
+++ lesson9/hometasks/hometask_1.py
@@ -1,5 +1,3 @@
-
-
 class Counter:
     def __init__(self, f):
         self.f = f
@@ -15,8 +13,36 @@ def square(x):
     return x * x
 
 
-print(square(5))
-print(square(10))
-print(square(11))
+# print(square(5))
+# print(square(10))
+# print(square(11))
+#
+# print(square.count)
 
-print(square.count)
+
+#  ---------------------------------------------------------------------------------------------------------------------
+
+# as example
+def decorator(f):
+    f.count = 0
+
+    def wrapper(*args, **kwargs):
+        f.count += 1
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+@decorator
+def func1():
+    return "hello"
+
+@decorator
+def func2():
+    return "world"
+
+
+print(func1())
+print(func1())
+print(func2())
+print(f'{func1.count} - {func2.count}')
