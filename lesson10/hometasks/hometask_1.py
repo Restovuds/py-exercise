b67355ee2ce4af5bdcc3501c11f2afe6a3735fbf
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-21 00:34:00 +0300

    Lesson 10 - Descriptor homework(1) check

--- lesson10/hometasks/hometask_1.py
+++ lesson10/hometasks/hometask_1.py
@@ -12,7 +12,12 @@ class UserCountryDescriptor:
         return self.country
 
     def __set__(self, instance, value):
-        raise AttributeError("This field is read-only | scenario 1 __set__")  # scenario 1
+        if not isinstance(value, str):
+            raise TypeError()
+
+        self.country = value
+
+        # raise AttributeError("This field is read-only | scenario 1 __set__")  # scenario 1
         # pass  # scenario 2
 
     def __delete__(self, instance):
@@ -23,7 +28,7 @@ class UserCountryDescriptor:
 class User:
     country = UserCountryDescriptor('Ukraine')
 
-    def __init__(self, name: str, surname: str):
+    def __init__(self, name: str, surname: str, country: str):
         if not isinstance(name, str):
             raise TypeError("Name must be a string")
         if not isinstance(surname, str):
@@ -31,18 +36,19 @@ class User:
 
         self.name = name
         self.surname = surname
+        self.country = country
 
     def __str__(self):
         return f'{self.name} {self.surname}, {self.country}'
 
 
-user_1 = User('John', 'Smith')
+user_1 = User('John', 'Smith', 'Polish')
 print(user_1)
 
 tmp = user_1.country
 print(tmp)
 
-# user_1.country = 'Germany'  # AttributeError: This field is read-only | scenario 1 __set__
-# print(user_1)
+user_1.country = 'Germany'  # AttributeError: This field is read-only | scenario 1 __set__
+print(user_1)
 
 # del user_1.country  # AttributeError: This field is read-only | scenario 1 __delete__
