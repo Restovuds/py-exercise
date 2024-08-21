d49b688aad49629f5d78ccfee8915a7c6474134a
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-21 20:59:38 +0300

    Lesson 10 - Descriptor, Exercise 3

--- lesson10/exercises/exercise_3.py
+++ lesson10/exercises/exercise_3.py
@@ -2,3 +2,30 @@
 Создайте класс LoggedProperty с дескриптором, который будет логировать доступ к атрибуту (как чтение, так и запись).
 Логирование может быть реализовано через простой вывод в консоль.
 """
+
+class LoggedProperty:
+    def __init__(self, name):
+        self.name = name
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+
+class User:
+    login = LoggedProperty('login')
+    password = LoggedProperty('password')
+
+    def __init__(self, login, pwd):
+        self.login = login
+        self.pwd = pwd
+
+    def __str__(self):
+        return f'{self.login} {self.pwd}'
+
+u1 = User('admin', '<PASSWORD>')
+print(u1)
+u2 = User('admin1', '<PASSWORD>1')
+print(u2)
