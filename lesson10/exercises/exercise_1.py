2cae0a756a91ed0f088eda9520e0836ae44294d2
Oleg Radchencko <oleg.rad4encko@gmail.com>
2024-08-21 20:17:09 +0300

    Lesson 10 - Descriptor, Exercise 1

---
+++ lesson10/exercises/exercise_1.py
@@ -0,0 +1,42 @@
+"""
+Создайте класс Temperature, который использует дескриптор для управления доступом к атрибуту температуры. Этот класс должен иметь следующие особенности:
+
+1 Атрибут celsius должен хранить температуру в градусах Цельсия.
+2 Когда температура устанавливается, она должна автоматически конвертироваться в Кельвины и Фаренгейты и сохраняться в соответствующих атрибутах.
+3 Реализуйте методы для получения температуры в Кельвинах и Фаренгейтах.
+"""
+
+
+
+class Temperature:
+    class Celsius:
+        def __get__(self, instance, owner):
+            return instance.__dict__['_celsius']
+
+        def __set__(self, instance, value):
+            instance.__dict__['_celsius'] = value
+            instance.__dict__['_kelvin'] = value + 273.15
+            instance.__dict__['_fahrenheit'] = value * 9 / 5 + 32
+
+    class Fahrenheit:
+        def __get__(self, instance, owner):
+            return instance.__dict__['_fahrenheit']
+
+    class Kelvin:
+        def __get__(self, instance, owner):
+            return instance.__dict__['_kelvin']
+
+    celsius = Celsius()
+    fahrenheit = Fahrenheit()
+    kelvin = Kelvin()
+
+    def __init__(self, celsius):
+        self.celsius = celsius
+
+    def __str__(self):
+        return f'{self.celsius} == {self.fahrenheit} == {self.kelvin}'
+
+
+temperature = Temperature(30)
+print(temperature)
+
