e07a699da959e735676f73796358496271df6e2e
restovuds <oleg.rad4encko@gmail.com>
2024-08-26 20:47:02 +0300

    # Single responsibility

---
+++ solid/main.py
@@ -0,0 +1,52 @@
+# solid
+
+# Single responsibility
+class Order:
+    def __init__(self):
+        self.items = []
+        self.quantities = []
+        self.prices = []
+        self.status = 'open'
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i, price in enumerate(self.prices):
+            total += self.quantities[i] * price
+        return total
+
+
+class PaymentProcessor:
+    def pay_debit(self, order: Order, security_code: str):
+        print('Processing debit payment type')
+        print(f'Verifying security code: {security_code}')
+        order.status = 'paid'
+
+    def pay_credit(self, order: Order, security_code: str):
+        print('Processing credit payment type')
+        print(f'Verifying security code: {security_code}')
+        order.status = 'paid'
+
+
+# order = Order()
+# order.add_item('Keyboard', 1, 50)
+# order.add_item('SSD', 1, 150)
+# order.add_item('USB cable', 2, 5)
+#
+# print(order.total_price())
+#
+# processor = PaymentProcessor()
+# processor.pay_debit(order, '0372846')
+
+
+
+
+
+
+
+
+
